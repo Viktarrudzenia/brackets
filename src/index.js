0692bb0d57fb2b51517835ade5ba8f94e598d42f
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-09-23 09:06:14 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  // Init what I must shield if find in bracketsConfig
+  let needShield = '.^$*+-?()[]{}\|';
+  // Init how much I will parse str (max)
+  let amount = str.length / 2;
+  let result = true;
+  let lettersForRegExp = '';
+
+  // Make letters for RegExp which i will search
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (i > 0) {
+      lettersForRegExp += '|';
+    }
+    for (let j = 0; j < 2; j++) {
+      if (needShield.indexOf(bracketsConfig[i][j]) !== -1) {
+        lettersForRegExp = lettersForRegExp + "\\" + bracketsConfig[i][j];
+      } else {
+        lettersForRegExp += bracketsConfig[i][j];
+      }
+    }
+  }
+
+  // Init regExp for search
+
+  let regExp = new RegExp(lettersForRegExp);
+
+  // Search coincidence and replace them with '' === (delete)
+
+  for (let i = 0; i <= amount; i++) {
+    result = str.replace(regExp, "");
+    str = result;
+  }
+  if (result.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
